adbd026e3bb878152afa3df13ba1b0b220430a10
luowanshun <luowanshun@gmail.com>
2019-10-18 19:51:58 +0800

    first release

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,7 +10,7 @@ func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "token"}
-	config.AllowOrigins = []string{"http://192.168.1.225:8080", "http://139.180.194.174", "http://127.0.0.1:8080", "http://127.0.0.1:8081"}
+	config.AllowOrigins = []string{"http://192.168.1.225:8080", "http://139.180.194.174", "http://127.0.0.1:8080", "http://127.0.0.1:8081", "https://www.katehq.com", "https://katehq.com"}
 	config.AllowCredentials = true
 	return cors.New(config)
 }
